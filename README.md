bda078a8db621d2fa5af4f87445efca134d80db2
crcrpar <kozukimasaki@KozukiMasaki-no-iMac.local>
2016-07-29 20:35:46 +0900

    edit
    edit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Morphological Analysis for Japanese text
+
+This is so simple program to analyze morphological of Japanese sentences and compute common 3 similarity coefficients.
+
+Be careful that your text encoding is **utf-8**.
+
+
+## Requirement
+- MeCab
--- execute.py
+++ execute.py
@@ -9,32 +9,14 @@ import sample as S
 import functions as F
 
 
-def morphoAnalysis(text):
-    m_a = MeCab.Tagger("-Ochasen")
-    result = m_a.parse(text)
-    #print("#####result#####\n{}".format(result))
-
-    return result
-
 def main():
     text_1 = S.sample_1
     text_2 = S.sample_2
-    text_3 = S.sample_3
-    text_4 = S.sample_4
-    text_5 = S.sample_5
-
-    analysis_1 = morphoAnalysis(text_1)
-    print("#{}#".format(text_1))
-    print("の形態素解析の結果は以下の通り．\n{}".format(analysis_1))
 
     model1_2 = F.MorphpAnalysis(text_1, text_2)
     coefficient1_2 = model1_2.coefficient(type = 2)
     print("#{}#\nと\n#{}#\nの係数".format(text_1, text_2))
     print(coefficient1_2)
-
-    model3_4 = F.MorphpAnalysis(text_3, text_4)
-    coefficient3_4 = model3_4.coefficient()
-    #print(coefficient3_4)
 
 
 if __name__ == "__main__":
